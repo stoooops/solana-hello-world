21b4fcc4da9d73293e2e42de3a24483513067643
Cory Gabrielsen <corygabrielsen@gmail.com>
2024-07-13 14:28:50 -0700

    Step 2: defining the account data

--- programs/solana-hello-world/src/lib.rs
+++ programs/solana-hello-world/src/lib.rs
@@ -14,3 +14,16 @@ pub mod solana_hello_world {
 
 #[derive(Accounts)]
 pub struct Initialize {}
+
+// #[account] is a custom Rust attribute that defines a Solana account.
+// Anchor essentially is telling the Rust compiler this is a Solana account.
+// There’s a lot of detail that Anchor abstracts away from us to be able to do this!
+// Pretty cool 😄!
+#[account]
+// pub struct Message is a Rust struct that defines the properties of a Message we want our program to interact with.
+// You’ll note we want to store the content of the most recent message, the timestamp when it was published, and the author.
+pub struct Message {
+    pub author: Pubkey,
+    pub timestamp: i64,
+    pub content: String,
+}
